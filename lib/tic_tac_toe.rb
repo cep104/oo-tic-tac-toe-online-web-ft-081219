cbf44252934bba93b6dae29005256ea2a5b4d9bb
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-18 21:51:13 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,76 @@
+class TicTacToe
+ def initialize 
+  @board = Array.new(9, " ")
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+  def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end 
+  
+  def input_to_index(index)
+   @index = index
+    @index.to_i-1
+  end
+  
+  def move(index,current_player)
+    @board[index] = current_player
+  end
+  
+  def position_taken?(index)
+!(@board[index].nil? || @board[index] == " ")
+  end
+  
+  def valid_move?(index)
+    index.between?(0,8) && @board[index].nil? || @board[index] == " "
+  end
+  
+  def turn_count 
+  turn = 0
+  @board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
+    end
+  end
+  return turn
+end
+
+def current_player
+  #if the turn count is an even number, that means O just went, so the next/current player is X
+  num_turns = turn_count
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
+
+def turn
+ puts "Please choose a number 1-9:"
+  input = gets.chomp
+  index = input_to_index(input)
+  if valid_move?(index)
+    move(index,current_player)
+    display_board
+  else 
+  input
+  end
+end
+  
+  
+end
